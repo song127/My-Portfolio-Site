c60c79a2931356dec6425d4f18204ac9399bd98f
song127 <36765469+song127@users.noreply.github.com>
2020-03-09 20:52:05 +0900

    Update All

--- js/jscript.js
+++ js/jscript.js
@@ -3,17 +3,20 @@ function goHere(num) {
   if(num === 0) { goTop(); } // 0이 인수로 올시 맨 위로 이동
   else { // 아닐 시 다른 곳으로 이동하기위한 좌표 알아내기
     $(".elements li").each(function (index, item) {
-      var position = $(item).offset().top;
-      if(num === index + 1) { moveToTag(position); return false; }
+      if(num === index + 1) {
+        var position = $(item).offset().top;
+        moveToTag(position);
+        return false; }
     });
   }
 }
 
 function moveToTag(position) {
-  var middle = position - (window.innerHeight / 2 - 450 / 2);
-  $('html, body').animate({scrollTop : middle}, 400);
+  // 이동할 메뉴가 가운데로 오도록 위치 조정
+  var middle = position - (window.innerHeight / 2 - 450 / 2 + 52);
+  $('html, body').animate({scrollTop : middle}, 500);
 }
 
 function goTop() {
-  $('html, body').animate({scrollTop : 0}, 400);
+  $('html, body').animate({scrollTop : 0}, 500);
 }
